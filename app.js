3a13495ecde6b373da07a28b4084947bc15f363a
rohit.verma@investec.co.za <rohit.verma@investec.co.za>
2019-07-29 11:50:13 +0200

    Port number changed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 // get all todos
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
